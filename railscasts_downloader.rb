3049ef31cf219417f34ba1b8c6923831f9a971fc
robert wallace <siliconchaos@gmail.com>
2010-07-25 02:52:42 -0500

    Updates to script in order to use axel instead of wget for downaloads

--- railscasts_downloader.rb
+++ railscasts_downloader.rb
@@ -1,25 +1,41 @@
 #!/usr/bin/ruby
 require 'rss'
 
-p 'Downloading rss index'
+# requires axel (http://axel.alioth.debian.org/) to be installed
+#   for osx use homebrew (http://mxcl.github.com/homebrew/) to install
+#     sudo brew install axel
+
+
+puts "\nDownloading rss index"
+puts "======================================================================="
 
 rss_string = open('http://feeds.feedburner.com/railscasts').read
 rss = RSS::Parser.parse(rss_string, false)
 videos_urls = rss.items.map { |it| it.enclosure.url }.reverse
 
 videos_filenames = videos_urls.map {|url| url.split('/').last }
-existing_filenames = Dir.glob('*.mov')
+incomplete_filenames = Dir.glob('*.mov.st').collect{|f| f.sub(/.st$/, '')}
+existing_filenames = Dir.glob('*.mov') - incomplete_filenames
 missing_filenames = videos_filenames - existing_filenames
-p "Downloading #{missing_filenames.size} missing videos"
 
+puts "\nResuming #{incomplete_filenames.size} inclompletd videos"
+puts "-----------------------------------------------------------------------"
+incomplete_videos_urls = videos_urls.select { |video_url| incomplete_filenames.any? { |filename| video_url.match filename } }
+
+incomplete_videos_urls.each do |video_url|
+  filename = video_url.split('/').last
+  puts "- #{filename}"
+  %x(axel -q #{video_url} )
+end
+
+puts "\nDownloading #{missing_filenames.size} missing videos"
+puts "-----------------------------------------------------------------------"
 missing_videos_urls = videos_urls.select { |video_url| missing_filenames.any? { |filename| video_url.match filename } }
-
-
 
 missing_videos_urls.each do |video_url|
   filename = video_url.split('/').last
-  p filename
-  p %x(wget -c #{video_url} -O #{filename}.tmp )
-  p %x(mv #{filename}.tmp #{filename} )
+  puts "- #{filename}"
+  %x(axel -q #{video_url} )
 end
-p 'Finished synchronization'
+puts "\n======================================================================="
+puts 'Finished synchronization'
